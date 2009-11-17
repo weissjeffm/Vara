e9457c5a48d45bcd0acbd5cbc8aac96ee4e690bd
Jeff Weiss <weissj.github@jweiss.com>
2009-11-17 18:56:32 -0500

    added dependsOnTest to match testng's dependsOnMethods (currently only one dep allowed)

--- src/clojure_ng.clj
+++ src/clojure_ng.clj
@@ -1,8 +1,13 @@
 (ns clojure-ng)
 (def test? nil)
+(def sorted-tests nil)
+(def dependency nil)
+
+;sample tests
+;------------------------------
 
 (defn #^{:test {:groups #{:group1 :group2} 
-                :runBefore :test2}}
+                :dependsOnTest #'test2}}
   test1
 	[]
 	(do (println "running test1")
@@ -14,28 +19,43 @@
 	(do(println "running test2")
 	(println "test2 complete")))
 	
-(defn #^{:test {:groups #{:group2 :group3}}}
+(defn #^{:test {:groups #{:group2 :group3}
+								:dependsOnTest #'test2}}
 	test3
 	[]
 	(do (println "running test3")
 	(throw (RuntimeException. "test failed!"))
 	(println "test3 complete")))
+
+(defn #^{:test {:groups #{:group1 :group3}
+								:dependsOnTest #'test3}}
+	test4
+	[]
+	(do (println "running test4")
+	;(throw (RuntimeException. "test failed!"))
+	(println "test4 complete")))
+
+;end sample tests
+;--------------------------------
 	
 (defn execute-test "returns results, with the test name, and 
 either :pass, :skip, or an exception, conj'd onto the end" 
 	[mytest results]
-	   (try (mytest) 
+		(let [dep (dependency mytest)]
+	   (if (and (not= dep nil) (not= ((results-map results) dep) :pass))
+	   	 :skip
+	     (try (mytest) 
           :pass
-     (catch Exception e e))) 
+       (catch Exception e e)))))
 		
 (defn run-tests-matching "Runs all tests using the testfilter-fn to filter
 out any tests that shouldn't be run.  Returns a map of test fn's to their result."
  ([]
  (run-tests-matching test?))
  ([testfilter]
-  (let [tests (filter 
+  (let [tests (sorted-tests (filter 
                  testfilter 
-                 (vals (ns-publics *ns*)))]
+                 (vals (ns-publics *ns*))))]
 	  (loop [remaining-tests tests
            results []]
 	  	(let [test (first remaining-tests)]
@@ -44,7 +64,8 @@ out any tests that shouldn't be run.  Returns a map of test fn's to their result
 	  	    (rest remaining-tests) 
 	  	    (conj results {test (execute-test test results)}))))))))
 
-;  	(hash-map (map execute-test tests))))
+(defn dependency [test]
+	(-> (meta test) :test :dependsOnTest))
 
 (defn results-map [results]
 	(into {} results))
@@ -57,3 +78,19 @@ out any tests that shouldn't be run.  Returns a map of test fn's to their result
 (defn test? [myfn]
 	(contains? (meta myfn) :test))
 	
+(defn compare-tests-order [arg1 arg2]
+	;first check args are right type
+	(if (not (and (var? arg1) (var? arg2))) 
+		(throw (IllegalArgumentException. (format "Both arguments should be a var. Got: %s, %s" arg1 arg2))))
+	(let [dep1 (dependency arg1)
+				dep2 (dependency arg2)]
+	(if (and (not (nil? dep1)) 
+	         (=  dep1 arg2)) ;run arg1 before arg2
+	   1
+	  (if (and (not (nil? dep2)) 
+	         (= dep2 arg1)) ;run arg2 before arg1
+			-1
+			0)))) ;don't care what order
+			
+(defn sorted-tests [tests]
+	(sort compare-tests-order tests))
