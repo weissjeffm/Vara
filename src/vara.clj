eb776c3d438700a6c0ab6da8ea992b61fcaced94
Jeff Weiss <weissj.github@jweiss.com>
2009-12-23 23:13:56 -0500

    more

--- src/vara.clj
+++ src/vara.clj
@@ -6,17 +6,21 @@
   (with-db mydb (create-document {:name name 
 				  :type "fieldTypeDef"
 				  :validator_fn (str fn-form)} (str "fieldTypeDef-" name))))
+(defn create-field [name type fn-form]
+  (with-db mydb (create-document {:name name 
+				  :type "field"
+				  :typedef_id (str "fieldTypeDef-" type)
+				  :validator_fn (str fn-form)} (str "field-" name))))
 
-(defn validate "takes a fieldname, a document, and a value.  Will extract the validator fn from the document,
-and call the function with the document and the value as arguments. Validator functions should always take
+(defn validate " Validator functions should always take
 these two arguments, and return a reason why the value is invalid, or nil if it's valid."
-  [field-name self value]
-  (let [validator (:validator_fn self)
-	result (try ((eval (read-string validator)) self value) 
-		    (catch Exception e 
-		      (throw (IllegalArgumentException. (format "Invalid validator function: %s" validator) e))))]
-    (if (not (nil? result)) 
-      (throw (Exception. (format "The field '%s' %s. Value given was %s" field-name result (str value)))))))
+  [fn-str field value]
+  (if-not (nil? fn-str)
+      (let [result (try ((eval (read-string fn-str)) field value) 
+			(catch Exception e 
+			  (throw (IllegalArgumentException. (format "Invalid validator function: %s" fn-str) e))))]
+	(if (not (nil? result)) 
+	  (throw (Exception. (format "The field '%s' %s. Value given was %s" (:name field) result (str value))))))))
 
 (defn validate-fields [typedef fieldmap ]
   (doseq [field-id (:field_ids typedef)]
@@ -24,8 +28,8 @@ these two arguments, and return a reason why the value is invalid, or nil if it'
 	  name (:name field)
 	  type (with-db mydb (get-document (:typedef_id field)))
 	  value (fieldmap (keyword name))]
-      (validate name type value)
-      (validate name field value))))
+      (validate (:validator_fn type) field value)
+      (validate (:validator_fn field) field value))))
 
 (defn create [type fieldmap]
   (comment (if (not (and (keyword? type) (map? fieldmap))) (throw (Exception. "Expecting a type keyword and "))))
