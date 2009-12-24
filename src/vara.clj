1567a765befad5acc4e0d03e2455c3b34dcda23f
Jeff Weiss <weissj.github@jweiss.com>
2009-12-24 15:52:42 -0500

    added compojure

--- src/vara.clj
+++ src/vara.clj
@@ -1,26 +1,35 @@
 (ns vara
-  (:use com.ashafa.clutch))
+  (:use  com.ashafa.clutch compojure)
+  (:import (bcrypt BCrypt)))
 (def mydb "vara")
 
-(defn create-field-typedef [name fn-form]
-  (with-db mydb (create-document {:name name 
-				  :type "fieldTypeDef"
-				  :validator_fn (str fn-form)} (str "fieldTypeDef-" name))))
-(defn create-field [name type fn-form]
-  (with-db mydb (create-document {:name name 
-				  :type "field"
-				  :typedef_id (str "fieldTypeDef-" type)
-				  :validator_fn (str fn-form)} (str "field-" name))))
+(defn create [docmap id]
+  (with-db mydb (create-document docmap id)))
 
-(defn validate " Validator functions should always take
+(defn create-field-typedef [name fn-form]
+  (create {:name name 
+	   :type "fieldTypeDef"
+	   :validator_fn (str fn-form)},
+	  (str "fieldTypeDef-" name)))
+
+(defn create-field [name type fn-form]
+  (create {:name name 
+	   :type "field"
+	   :typedef_id (str "fieldTypeDef-" type),
+	   :validator_fn (str fn-form)} (str "field-" name)))
+
+(defn validate-field " Validator functions should always take
 these two arguments, and return a reason why the value is invalid, or nil if it's valid."
   [fn-str field value]
-  (if-not (nil? fn-str)
-      (let [result (try ((eval (read-string fn-str)) field value) 
-			(catch Exception e 
-			  (throw (IllegalArgumentException. (format "Invalid validator function: %s" fn-str) e))))]
-	(if (not (nil? result)) 
-	  (throw (Exception. (format "The field '%s' %s. Value given was %s" (:name field) result (str value))))))))
+  (if fn-str
+    (let [validation-error (try ((eval (read-string fn-str)) field value) 
+		      (catch Exception e 
+			(throw (IllegalArgumentException. (format "Invalid validator function: %s" fn-str) e))))]
+      (if validation-error 
+	(throw (Exception. (format "The field '%s' %s. Value given was %s" 
+				   (:name field)
+				   validation-error 
+				   (str value))))))))
 
 (defn validate-fields [typedef fieldmap ]
   (doseq [field-id (:field_ids typedef)]
@@ -28,11 +37,19 @@ these two arguments, and return a reason why the value is invalid, or nil if it'
 	  name (:name field)
 	  type (with-db mydb (get-document (:typedef_id field)))
 	  value (fieldmap (keyword name))]
-      (validate (:validator_fn type) field value)
-      (validate (:validator_fn field) field value))))
+      (validate-field (:validator_fn type) field value)
+      (validate-field (:validator_fn field) field value))))
 
-(defn create [type fieldmap]
-  (comment (if (not (and (keyword? type) (map? fieldmap))) (throw (Exception. "Expecting a type keyword and "))))
+(defn create-user [userid realname email password]
+  (let [hash (bcrypt.BCrypt/hashpw password (bcrypt.BCrypt/gensalt))]
+    (create {:type "user"
+	     :userid userid
+	     :email email
+	     :password hash
+	     :realname realname},
+	    (str "user-" userid))))
+
+(defn create-record [type fieldmap]
   (let [typestr (name type)
 	typedef-id (str "recordTypeDef-" typestr)
 	typedef (with-db mydb 
@@ -42,3 +59,15 @@ these two arguments, and return a reason why the value is invalid, or nil if it'
     (with-db mydb (create-document (assoc fieldmap 
 				     :type typestr
 				     :typedef_id typedef-id)))))
+
+(defroutes my-app
+  (GET "/"
+    (html [:h1 "Hello World"]))
+  (ANY "*"
+    (page-not-found)))
+
+
+(defn start-compojure []
+  (run-server {:port 8080}
+	     "/*" 
+	     (servlet my-app) ))
